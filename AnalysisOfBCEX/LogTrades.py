be02e995e01d5c574eb86e8a38367687dbbe9b66
Josh Varty <joshvarty@gmail.com>
2018-08-07 01:40:12 -0400

    Init and log trades.

---
+++ AnalysisOfBCEX/LogTrades.py
@@ -0,0 +1,35 @@
+import ccxt
+import time 
+
+
+def logtrades():
+
+    hour_ago = int(time.time() * 1000) - 60 * 1000 
+    exchange = getattr(ccxt, "bcex")()
+    pair = "BTC/CKUSD"
+
+    last_time = int(time.time() * 1000) - (60 * 1000  * 1000)
+
+    with open('trades.txt', 'a') as my_log:
+
+        while True:
+
+            trades = exchange.fetch_trades(pair, since = last_time)
+
+            for trade in trades:
+
+                my_log.write(trade['timestamp'])
+                my_log.write(",")
+                my_log.write(trade['datetime'])
+                my_log.write(",")
+                my_log.write(trade['price'])
+                my_log.write(",")
+                my_log.write(trade['amount'])
+                my_log.write("\n")
+
+                last_time = trade['timestamp']
+
+                time.sleep(60) #sleep one minute
+
+if __name__ == "__main__":
+    logtrades();
